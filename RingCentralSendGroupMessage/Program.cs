634bb64de8a15501c599fb8d5042e58df549b1c7
Tyler Long <tyler4long@gmail.com>
2020-03-19 16:42:28 -0700

    Initial commit

---
+++ RingCentralSendGroupMessage/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using RingCentral;
+
+namespace RingCentralSendGroupMessage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var rc = new RestClient(
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_CLIENT_SECRET"),
+                Environment.GetEnvironmentVariable("RINGCENTRAL_SERVER_URL")
+            ))
+            {
+                Task.Run(async () =>
+                {
+                    await rc.Authorize(
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME"),
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_EXTENSION"),
+                        Environment.GetEnvironmentVariable("RINGCENTRAL_PASSWORD")
+                    );
+
+                    var r = await rc.Restapi().Account().Extension().Sms().Post(new CreateSMSMessage
+                    {
+                        from = new MessageStoreCallerInfoRequest
+                        {
+                            phoneNumber = Environment.GetEnvironmentVariable("RINGCENTRAL_USERNAME")
+                        },
+                        // multiple recipient, so this is actually an MMS
+                        // user received this message will be able to see all the recipient numbers
+                        // so this is not a good way to send massive notifications.
+                        to = new []
+                        {
+                            new MessageStoreCallerInfoRequest
+                            {
+                                phoneNumber = Environment.GetEnvironmentVariable("RINGCENTRAL_RECIPIENT_1")
+                            },
+                            new MessageStoreCallerInfoRequest
+                            {
+                                phoneNumber = Environment.GetEnvironmentVariable("RINGCENTRAL_RECIPIENT_2")
+                            }
+                        },
+                        text = "hello world"
+                    });
+                    Console.WriteLine(r);
+                }).GetAwaiter().GetResult();
+            }
+        }
+    }
+}
